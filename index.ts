a7391a57452e50338dbe95b78183243e6fd736e2
HASNAIN RAZA <139609308+hasnaingithubrit@users.noreply.github.com>
2023-07-26 08:15:37 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+let message : string = "Hey Abu Hurairah,I have started learning TYPESCRIPT."
+let  a:string = "Hasnain Raza";
+let n1: number = 33;
+let n2: number = 11;
+let n3: number = 22;
+let n4: number = 45;
+let n5: number = 77;
+let n6: number = 32;
+let n7: number = 12;
+let n8: number = 21;
+let n9: number = 15;
+let n10:number = 33;
+let sum =n1+n2+n3+n4+n5+n6+n7+n8+n9+n10;
+let subtraction = n1-n2-n3-n4-n5-n6-n7-n8-n9-n10;
+let multiply = n1*n2*n3*n4*n5*n6*n7*n8*n9*n10;
+let division = n5/n10;
+
+console.log(message);
+console.log(a);
+console.log(sum);
+console.log(subtraction);
+console.log(multiply);
+console.log(division);
